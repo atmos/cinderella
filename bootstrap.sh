b7314bb94301d96e059be56a6a018e50281cccf1
Corey Donohoe <atmos@atmos.org>
2010-05-23 17:53:07 -0700

    --no-rdoc --no-ri by default

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 which gem | grep -q rvm
 if [ $? -eq 0 ]; then
   gem uninstall cider
-  gem install   cider
+  gem install   cider --no-rdoc --no-ri
 else
   sudo gem uninstall cider
-  sudo gem install   cider
+  sudo gem install   cider --no-rdoc --no-ri
 fi
 hash -r
 cider
