da54e7573c7678a90e275a45bc72e62ba52593d6
Corey Donohoe <atmos@atmos.org>
2010-10-05 22:28:10 -0700

    try this for bootstrapping folks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,43 +1,39 @@
 echo "Ensuring we have the latest version of cinderella installed"
-echo "Run started `date`" >> ~/.cinderella.bootstrap.log 2>&1
-which ruby >> ~/.cinderella.bootstrap.log 2>&1
-which gem  >> ~/.cinderella.bootstrap.log 2>&1
+mkdir -p ~/.cinderella
+echo "Run started `date`" >> ~/.cinderella/bootstrap.log 2>&1
+which ruby >> ~/.cinderella/bootstrap.log 2>&1
+which gem  >> ~/.cinderella/bootstrap.log 2>&1
 
 which gem | grep -q rvm
 if [ $? -eq 0 ]; then
-  gem uninstall cinderella -aIx              >> ~/.cinderella.bootstrap.log 2>&1
-  gem install   cinderella --no-rdoc --no-ri >> ~/.cinderella.bootstrap.log 2>&1
+  gem uninstall cinderella -aIx              >> ~/.cinderella/bootstrap.log 2>&1
+  gem install   cinderella --no-rdoc --no-ri >> ~/.cinderella/bootstrap.log 2>&1
 else
-  sudo gem uninstall cinderella -aIx              >> ~/.cinderella.bootstrap.log 2>&1
-  sudo gem install   cinderella --no-rdoc --no-ri >> ~/.cinderella.bootstrap.log 2>&1
+  sudo gem uninstall cinderella -aIx              >> ~/.cinderella/bootstrap.log 2>&1
+  sudo gem install   cinderella --no-rdoc --no-ri >> ~/.cinderella/bootstrap.log 2>&1
 fi
 
 echo "Cinderella installed successfully"
 
-hash -r
-cinderella
-if [ "$?" -eq "0" ]; then
-  if [ -d ~/.cinderella ]; then
-    mv ~/.cinderella.bootstrap.log ~/.cinderella
-  fi
-  source ~/.cinderella.profile
+function run_cinderella {
   hash -r
-else
-  echo "Something went wonky, retrying"
+  if [ -f ~/.cinderella.profile ]; then
+    source ~/.cinderella.profile
+  fi
   cinderella
   if [ "$?" -eq "0" ]; then
-    source ~/.cinderella.profile
-    hash -r
-  else
-    echo "Something went wonky again, retrying once more"
-    cinderella
-    if [ "$?" -eq "0" ]; then
-      source ~/.cinderella.profile
-      hash -r
-    else
-      cat ~/.cinderella.bootstrap.log
-      echo ""
-      echo "Something went wonky with the install. :("
-    fi
+    exit 0
   fi
-fi
+}
+
+# try cinderella three times just in case shit gets weird
+run_cinderella
+run_cinderella
+run_cinderella
+
+cat ~/.cinderella/bootstrap.log
+echo ""
+echo "Something went wrong with the install. :("
+echo "Dump this log into a gist and link to me to it"
+echo "http://github.com/atmos/cinderella/issues"
+echo "Sorry it failed. :("
